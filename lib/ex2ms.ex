625435b893b92b81a6af103cd43e06fe52f5b44b
Eric Meadows-Jonsson <eric.meadows.jonsson@gmail.com>
2013-06-29 23:32:50 +0200

    Add expressions for guards and body

--- lib/ex2ms.ex
+++ lib/ex2ms.ex
@@ -1,15 +1,25 @@
 defmodule Ex2ms do
-  @bool_function [
+  @bool_functions [
     :is_atom, :is_float, :is_integer, :is_list, :is_number, :is_pid, :is_port,
     :is_reference, :is_tuple, :is_binary, :is_function, :is_record,
     :and, :or, :not, :xor, :andalso, :orelse ]
 
-  @guard_function @bool_function ++ [
+  @guard_functions @bool_functions ++ [
     :abs, :element, :hd, :count, :node, :round, :size, :tl, :trunc, :+, :-, :*,
     :div, :rem, :band, :bor, :bxor, :bnot, :bsl, :bsr, :>, :>=, :<, :<=, :===,
     :==, :!==, :!=, :self ]
 
   @elixir_erlang [ ===: :"=:=", !==: :"=/=", <=: :"=<" ]
+
+  Enum.map(@guard_functions, fn(atom) ->
+    defp is_guard_function(unquote(atom)), do: true
+  end)
+  defp is_guard_function(_), do: false
+
+  Enum.map(@elixir_erlang, fn({ elixir, erlang }) ->
+    defp map_elixir_erlang(unquote(elixir)), do: unquote(erlang)
+  end)
+  defp map_elixir_erlang(atom), do: atom
 
   defrecord State, vars: [], count: 0
 
@@ -22,7 +32,7 @@ defmodule Ex2ms do
   end
 
   defmacro fun(_) do
-    raise ArgumentError, message: "invalid args to fun"
+    raise ArgumentError, message: "invalid args to matchspec"
   end
 
   defmacrop is_literal(term) do
@@ -39,18 +49,56 @@ defmodule Ex2ms do
     { head, conds, body }
   end
 
-  defp translate_body(_, _state) do
-    []
+  defp translate_body({ :__block__, _, exprs }, state) when is_list(exprs) do
+    Enum.map(exprs, translate_cond(&1, state))
   end
 
-  defp translate_conds(_, _state) do
-    []
+  defp translate_body(expr, state) do
+    [ translate_cond(expr, state) ]
   end
 
-  defp translate_head([{ :when, _, [param, conds] }]) do
+  defp translate_cond({ var, _, nil }, state) when is_atom(var) do
+    if match_var = state.vars[var] do
+      :"#{match_var}"
+    else
+      raise ArgumentError, message: "variable `#{var}` is unbound in matchspec"
+    end
+  end
+
+  defp translate_cond({ left, right }, state) do
+    translate_cond({ :{}, [], [left, right] }, state)
+  end
+
+  defp translate_cond({ :{}, [], list }, state) when is_list(list) do
+    { Enum.map(list, translate_cond(&1, state)) |> list_to_tuple }
+  end
+
+  defp translate_cond({ fun, _, args }, state) when is_atom(fun) and is_list(args) do
+    if is_guard_function(fun) do
+      match_args = Enum.map(args, translate_cond(&1, state))
+      match_fun = map_elixir_erlang(fun)
+      [match_fun|match_args] |> list_to_tuple
+    else
+      raise ArgumentError, message: "illegal function `#{fun}` in matchspec"
+    end
+  end
+
+  defp translate_cond(list, state) when is_list(list) do
+    Enum.map(list, translate_cond(&1, state))
+  end
+
+  defp translate_cond(literal, _state) when is_literal(literal) do
+    literal
+  end
+
+  defp translate_cond(_, _state) do
+    raise ArgumentError, message: "illegal expression in matchspec"
+  end
+
+  defp translate_head([{ :when, _, [param, cond] }]) do
     { head, state } = translate_param(param)
-    conds= translate_conds(conds, state)
-    { head, conds, state }
+    cond = translate_cond(cond, state)
+    { head, [cond], state }
   end
 
   defp translate_head([param]) do
