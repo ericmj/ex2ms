8f6f36c647bc686dc3d869d5ed1ad25af89f194b
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-02-24 22:52:20 +0100

    Release v1.5.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ex2ms.Mixfile do
 
   def project do
     [ app: :ex2ms,
-      version: "1.4.0",
+      version: "1.5.0",
       elixir: "~> 1.0",
       description: description(),
       package: package(),
